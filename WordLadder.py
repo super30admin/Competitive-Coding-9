cbc67d66dc605d676f5ba6318a2ecc4fbf2100fc
amby2964 <75859704+amby2964@users.noreply.github.com>
2021-02-23 00:19:54 -0700

    Create WordLadder.py

---
+++ WordLadder.py
@@ -0,0 +1,39 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wl = set(wordList)
+        if(not endWord in wl):
+            return 0
+        # DFS backtracking- TLE
+        @cache
+        def diff(w1,w2):
+            flag = False
+            i = 0
+            while(i<len(w1)):
+                if(w1[i]!=w2[i]):
+                    if(flag==False):
+                        flag = True
+                    else:
+                        return False
+                i+=1
+            return True
+        
+        min1 = float("inf")
+        def helper(w,wl,c):
+            o = set()
+            for i in wl:
+                if(diff(w,i)==True):
+                    if(i==endWord):
+                        nonlocal min1
+                        if(c+1<min1):
+                            min1=c+1
+                            return
+                    o.add(i)
+            for i in o:
+                wl.remove(i)
+                helper(i,wl,c+1)
+                wl.add(i)
+            
+        helper(beginWord, wl, 1)
+        if(min1==float("inf")):
+            return 0
+        return min1
