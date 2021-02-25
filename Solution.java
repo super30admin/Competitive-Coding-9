71f167acb1f696152f76a296e78576094a0e4c72
tholerohit <76415875+tholerohit@users.noreply.github.com>
2021-02-25 18:50:28 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,5 +1,42 @@
-package cc9;
-
-public class Solution {
-
-}
+//Time Complexity:O(n*m)
+//Space Complexity:O(m)
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set=new HashSet(wordList);
+        if(!set.contains(endWord)){
+            return 0;
+        }
+        Queue<String> q=new LinkedList();
+        int level=1;
+        q.add(beginWord);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                String curr=q.poll();
+                char[] currword=curr.toCharArray();
+                for(int j=0;j<currword.length;j++){
+                    char temp=currword[j];
+                    for(char c='a';c<='z';c++){
+                        if(currword[j]==c){
+                            continue;
+                        }
+                        currword[j]=c;
+                        String newword=String.valueOf(currword);
+                        if(set.contains(newword)){
+                            if(newword.equals(endWord)){
+                                return level+1;
+                            }
+                            else{
+                                q.add(newword);
+                                set.remove(newword);
+                            }
+                        }
+                        currword[j]=temp;
+                    }
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+} 
