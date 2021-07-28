73f95c1a060cbcff227027c52c2357e20db50b16
krishnaattapu <krishna.attapu@gmail.com>
2021-07-28 01:03:55 -0500

    Competitive-Coding-9 completed

--- WordLadder.java
+++ WordLadder.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(M pow 2 *N)// M is length of word and N is number of words
+// Space Complexity : O(M pow 2 * N)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -8,39 +13,52 @@ import java.util.Queue;
 public class WordLadder {
 	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
-		Map<String, List<String>> map = new HashMap<>();
+		// Since all words are of same length.
+		int L = beginWord.length();
 
-		int wordLength = beginWord.length();
-		
-		for (String word : wordList) {
-			for (int i = 0; i < wordLength; i++) {
-				map.compute(word.substring(0, i) + "*" + word.substring(i + 1, wordLength),
-						(k, v) -> v == null ? new ArrayList<String>() : v).add(word);
+		// Dictionary to hold combination of words that can be formed,
+		// from any given word. By changing one letter at a time.
+		Map<String, List<String>> allComboDict = new HashMap<>();
+
+		wordList.forEach(word -> {
+			for (int i = 0; i < L; i++) {
+				// Key is the generic word
+				// Value is a list of words which have the same intermediate generic word.
+				String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+				List<String> transformations = allComboDict.getOrDefault(newWord, new ArrayList<>());
+				transformations.add(word);
+				allComboDict.put(newWord, transformations);
 			}
-		}
+		});
 
-		Queue<String> queue = new LinkedList<>();
+		// Queue for BFS
+		Queue<Pair<String, Integer>> Q = new LinkedList<>();
+		Q.add(new Pair(beginWord, 1));
+
+		// Visited to make sure we don't repeat processing same word.
 		Map<String, Boolean> visited = new HashMap<>();
-
-		queue.offer(beginWord);
 		visited.put(beginWord, true);
 
-		int level = 0;
+		while (!Q.isEmpty()) {
+			Pair<String, Integer> node = Q.remove();
+			String word = node.getKey();
+			int level = node.getValue();
+			for (int i = 0; i < L; i++) {
 
-		while (!queue.isEmpty()) {
-			level++;
-			String word = queue.poll();
-			for (int i = 0; i < wordLength; i++) {
-				String newWord = word.substring(0, i) + "*" + word.substring(i + 1, wordLength);
+				// Intermediate words for current word
+				String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
 
-				if (map.containsKey(newWord)) {
-					for (String adjWord : map.get(newWord)) {
-						if (adjWord.equals(endWord))
-							return level;
-						if (!visited.containsKey(adjWord)) {
-							visited.put(adjWord, true);
-							queue.offer(adjWord);
-						}
+				// Next states are all the words which share the same intermediate state.
+				for (String adjacentWord : allComboDict.getOrDefault(newWord, new ArrayList<>())) {
+					// If at any point if we find what we are looking for
+					// i.e. the end word - we can return with the answer.
+					if (adjacentWord.equals(endWord)) {
+						return level + 1;
+					}
+					// Otherwise, add it to the BFS Queue. Also mark it visited
+					if (!visited.containsKey(adjacentWord)) {
+						visited.put(adjacentWord, true);
+						Q.add(new Pair(adjacentWord, level + 1));
 					}
 				}
 			}
@@ -48,9 +66,9 @@ public class WordLadder {
 
 		return 0;
 	}
-	
+
 	public static void main(String[] args) {
-		List<String> dict=new ArrayList<>();
+		List<String> dict = new ArrayList<>();
 		dict.add("hot");
 		dict.add("dog");
 		dict.add("dot");
