9b331c6d2fc36a9b64798f50c09f4d75e1e0f770
AayushSanghai <61236166+AayushSanghai@users.noreply.github.com>
2022-06-01 03:50:03 -0500

    Hi Team, Competitive Coding 9 done. Please Review

---
+++ MinimumCostForTickets.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    // Time Complexity :0(n) where n is the max no. of days for the tickets
+// Space Complexity :0(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+// Idea here is to check for each day in days untill last day of travel
+// if a person is travelling on certain day what is the minimum cost applied if they perchased passes
+// of 1 day 1 day ago, of 7 day 7 day ago and of 30 day 30 day ago
+// whichever is minumum gives current travel min expence
+// last day will give total minimum cost.
+
+    public int mincostTickets(int[] days, int[] costs) {
+        Set<Integer> set = new HashSet<>();
+        for(int day: days) {
+            set.add(day);
+        }
+        int [] dp= new int[days[days.length-1]+1];
+        for(int i = 1; i<dp.length; i++) {
+            if(!set.contains(i)) dp[i] = dp[i-1];
+            else {
+                dp[i] = Math.min((dp[i-1] + costs[0]), Math.min(
+                        dp[Math.max(i-7,0)] + costs[1],
+                        dp[Math.max(i-30,0)] + costs[2]));
+            }
+        }
+        return dp[dp.length-1];
+    }
+}
