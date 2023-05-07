6e714eb0dda39ec74853354a3517cf0211ea4f2c
tirthaj.rikame@gmail.com <tirthaj.rikame@gmail.com>
2023-05-07 12:55:44 -0400

    Completed Design-9. Please review.

---
+++ Word Ladder.py
@@ -0,0 +1,21 @@
+class Solution:
+    # Time O(m**2 * n)
+    # Space O(N)
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # BFS APPROACH
+        dq = deque()
+        dq.append((beginWord, 1))
+        hset = set(wordList)
+
+        while dq:
+            curr, level = dq.popleft()
+            if curr == endWord:
+                return level
+            for i in range(len(curr)):
+                for j in range(ord('a'), ord('z') + 1):
+                    word = curr[:i] + chr(j) + curr[i + 1:]
+                    if word in hset:
+                        dq.append((word, level + 1))
+                        hset.remove(word)
+
+        return 0
