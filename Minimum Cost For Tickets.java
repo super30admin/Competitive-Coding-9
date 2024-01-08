6f3f5d5f83d21489b2b94ffa8c94a429547af26a
Prakhar Srivastava <prakharsrivastava@Prakhars-Air.attlocal.net>
2024-01-08 11:19:55 -0800

    Completed Competitive-Coding-9

---
+++ Minimum Cost For Tickets.java
@@ -0,0 +1,30 @@
+// Time Complexity :O(N)
+// Space Complexity :O(N)  //N is number of days
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        Queue<int[]> last7 = new LinkedList<>();
+        Queue<int[]> last30= new LinkedList<>();
+        int cost=0;
+
+        for(int d: days)
+        {
+            while(!last7.isEmpty() && last7.peek()[0]+7 <=d)
+            {
+                last7.poll();
+            }
+            while(!last30.isEmpty() && last30.peek()[0]+30<= d)
+            {
+                last30.poll();
+            }
+            last7.offer(new int[]{d,cost+costs[1]});
+            last30.offer(new int[]{d,cost+costs[2]});
+            cost= Math.min(cost+costs[0],Math.min(last7.peek()[1],last30.peek()[1]));
+        }
+        return cost;
+    }
+}
